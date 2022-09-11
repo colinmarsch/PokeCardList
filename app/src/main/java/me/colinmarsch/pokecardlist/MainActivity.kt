1fbe7bdd8a364a1f7da7bf4276e62997cc388d9b
Colin Marsch <colinmarsch@gmail.com>
2022-09-10 22:03:20 -0400

    Set up navigaton controller in MainActivity

--- app/src/main/java/me/colinmarsch/pokecardlist/MainActivity.kt
+++ app/src/main/java/me/colinmarsch/pokecardlist/MainActivity.kt
@@ -2,10 +2,55 @@ package me.colinmarsch.pokecardlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.remember
+import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            val navController = rememberNavController()
+            NavHost(
+                navController = navController,
+                startDestination = "main_set_list_screen",
+            ) {
+                composable("main_set_list_screen") {
+                    // TODO define the composable screen here
+                }
+
+                composable(route = "sub_set_list_screen", arguments = listOf(
+                    navArgument("parentSetName") {
+                        type = NavType.StringType
+                    }
+                )) {
+                    val parentSetName = it.arguments?.get("parentSetName")
+                    // TODO define the composable screen here
+                }
+
+                composable(route = "set_card_list_screen", arguments = listOf(
+                    navArgument("parentSetName") {
+                        type = NavType.StringType
+                    }
+                )) {
+                    val parentSetName = it.arguments?.get("parentSetName")
+                    // TODO define the composable screen here
+                }
+
+                composable(route = "card_detail_screen", arguments = listOf(
+                    navArgument("cardName") {
+                        type = NavType.StringType
+                    }
+                )) {
+                    val cardName = it.arguments?.get("cardName")
+                    // TODO define the composable screen here
+                }
+            }
+        }
     }
 }
