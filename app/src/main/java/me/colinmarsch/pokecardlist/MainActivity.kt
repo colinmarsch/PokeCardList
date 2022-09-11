f8961aeebe04385d94a0174dbd9ef2a06c2ecabd
Colin Marsch <colinmarsch@gmail.com>
2022-09-10 22:17:15 -0400

    Setup Dagger + Hilt bindings

--- app/src/main/java/me/colinmarsch/pokecardlist/MainActivity.kt
+++ app/src/main/java/me/colinmarsch/pokecardlist/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/me/colinmarsch/pokecardlist/PokeCardListApp.kt
@@ -0,0 +1,8 @@
+package me.colinmarsch.pokecardlist
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PokeCardListApp : Application() {
+}
