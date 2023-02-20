f4205dadb4b84b39352c07ea4d98c8bcfd7bfab2
Colin Marsch <cmarsch@squareup.com>
2023-02-20 14:18:33 -0500

    Add loading spinners when cards are loading

--- app/src/main/java/me/colinmarsch/pokecardlist/ui/CardsListScreen.kt
+++ app/src/main/java/me/colinmarsch/pokecardlist/ui/CardsListScreen.kt
@@ -1,17 +1,18 @@
 package me.colinmarsch.pokecardlist.ui
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -29,72 +30,76 @@ import coil.compose.AsyncImage
 import me.colinmarsch.pokecardlist.data.CardListViewModel
 import me.colinmarsch.pokecardlist.db.Card
 
-@Composable
-fun CardsListScreen(
-    parentSet: String,
-    navController: NavController,
-    viewModel: CardListViewModel = hiltViewModel(),
+@Composable fun CardsListScreen(
+  parentSet: String,
+  navController: NavController,
+  viewModel: CardListViewModel = hiltViewModel(),
 ) {
-    Surface(
-        color = MaterialTheme.colors.background,
-        modifier = Modifier.fillMaxSize(),
-    ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(text = parentSet)
-            CardList(parentSet, navController, viewModel)
-        }
+  val cardList by remember { viewModel.cardsInSet(parentSet) }
+
+  Surface(
+    color = MaterialTheme.colors.background,
+    modifier = Modifier.fillMaxSize(),
+  ) {
+    if (cardList.isEmpty()) {
+      Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        CircularProgressIndicator()
+      }
+    } else {
+      Column(
+        horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center
+      ) {
+        Text(text = parentSet)
+        CardList(parentSet, navController, cardList)
+      }
     }
+  }
 }
 
-@Composable
-fun CardList(
-    parentSet: String,
-    navController: NavController,
-    viewModel: CardListViewModel = hiltViewModel(),
+@Composable fun CardList(
+  parentSet: String,
+  navController: NavController,
+  cardList: List<Card>,
 ) {
-    val cardList by remember { viewModel.cardsInSet(parentSet) }
+  LazyVerticalGrid(
+    columns = GridCells.Fixed(2),
+    contentPadding = PaddingValues(16.dp),
+    verticalArrangement = Arrangement.spacedBy(16.dp),
+    horizontalArrangement = Arrangement.spacedBy(16.dp),
+  ) {
+    val filteredCards = cardList.filter { it.parentSetId == parentSet }
+    val setCount = filteredCards.size
 
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        contentPadding = PaddingValues(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        horizontalArrangement = Arrangement.spacedBy(16.dp),
-    ) {
-        val filteredCards = cardList.filter { it.parentSetId == parentSet }
-        val setCount = filteredCards.size
-
-        items(setCount) { index ->
-            CardCard(filteredCards[index], navController)
-        }
+    items(setCount) { index ->
+      CardCard(filteredCards[index], navController)
     }
+  }
 }
 
-@OptIn(ExperimentalMaterialApi::class)
-@Composable
-fun CardCard(
-    card: Card,
-    navController: NavController,
+@OptIn(ExperimentalMaterialApi::class) @Composable fun CardCard(
+  card: Card,
+  navController: NavController,
 ) {
-    Card(
-        onClick = {
-            // TODO handle navigation here
-        },
-        backgroundColor = MaterialTheme.colors.secondary,
-        modifier = Modifier
-            .fillMaxWidth()
-            .wrapContentHeight(),
+  Card(
+    onClick = {
+      // TODO handle navigation here
+    },
+    backgroundColor = MaterialTheme.colors.secondary,
+    modifier = Modifier
+      .fillMaxWidth()
+      .wrapContentHeight(),
+  ) {
+    Column(
+      horizontalAlignment = Alignment.CenterHorizontally,
+      modifier = Modifier.padding(16.dp),
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.padding(16.dp),
-        ) {
-            AsyncImage(
-                model = card.smallImageUrl,
-                contentDescription = card.name,
-                modifier = Modifier.fillMaxHeight(0.66f),
-                contentScale = ContentScale.Fit,
-            )
-            Text(text = card.name ?: "", )
-        }
+      AsyncImage(
+        model = card.smallImageUrl,
+        contentDescription = card.name,
+        modifier = Modifier.fillMaxHeight(0.66f),
+        contentScale = ContentScale.Fit,
+      )
+      Text(text = card.name ?: "")
     }
+  }
 }
